49997457e2a8492d36fcf09a1fab4a72639329f7
xodyd7895 <91954212+xodyd7895@users.noreply.github.com>
2022-01-25 19:55:16 +0900

    Update Earth.java

--- exampleForCh12/Earth.java
+++ exampleForCh12/Earth.java
@@ -1,7 +1,7 @@
 package exampleForCh12;
 
 public class Earth {
-	static final double EARTH_RADIUS = 6500;
+	static final double EARTH_RADIUS = 6300;
 	static final double EARTH_SURFACE_AREA;
 	
 	static {

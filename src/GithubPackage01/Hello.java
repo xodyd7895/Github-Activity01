acae1217504b32017921e5de7b14c9340000d0e2
xodyd7895 <91954212+xodyd7895@users.noreply.github.com>
2022-01-30 18:00:24 +0900

    Update Hello.java

--- src/GithubPackage01/Hello.java
+++ src/GithubPackage01/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
+		System.out.print("Github"); // 줄띄움x
 		//Practice for Github
 		
 	}
